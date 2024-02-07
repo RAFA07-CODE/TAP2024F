1a7761c55ac0e09cf99ce55d12d82bbff705742b
Rafa Villegas <78750280+RAFA07-CODE@users.noreply.github.com>
2024-02-06 18:26:34 -0600

    Screen cuadro magico

---
+++ src/main/java/com/example/tap2024/vistas/CuadroMagico.java
@@ -0,0 +1,16 @@
+package com.example.tap2024.vistas;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class CuadroMagico extends Stage {
+    private Scene escena
+    public CuadroMagico(){
+        this.setTitle("Cuadro Magico");
+        this.setScene(new Scene(new Button("Da click")));
+        this.show();
+    }
+    private void CrearUI(){
+        escena = new Scene(new Button("Da Click"));
+    }
+}
